ef85048f55ef8f242b9decaece9365efd1e36bef
Mirko Ledda <mko.ledda@gmail.com>
2019-02-16 01:16:31 -0800

    updated CV + fixed bib

---
+++ assets/bib/header.md
@@ -0,0 +1,6 @@
+---
+layout: single
+title: "Publications"
+permalink: /pages/publications.html
+author_profile: false
+---
---
+++ get_publications_page.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+bibtex2html -s assets/bib/mylib.bst -d -r -o _pages/publications assets/bib/mylib.bib
+cp _pages/publications.html _pages/publications2.html
+cp assets/bib/header.md _pages/publications2.html
+cat _pages/publications.html >> _pages/publications2.html
+sed 's/_pages/{{ site.baseurl }}\/assets\/bib/g' _pages/publications2.html > _pages/publications.html
+sed 's/_pages/{{ site.baseurl }}\/assets\/bib/g' _pages/publications_bib.html > _pages/publications_bib2.html
+
+rm _pages/publications2.html
+mv _pages/publications_bib2.html _pages/publications_bib.html
+mv _pages/publications_bib.html assets/bib/publications_bib.html
